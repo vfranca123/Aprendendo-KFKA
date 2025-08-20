8b5323539db315bed1da5e3e9f19e360474e07e7
Vitor Ferreira frança <vitorferreirafranca@gmail.com>
2025-08-20 11:05:52 -0300

    first commit

---
+++ Produtor/Config/BrokerConfig.cs
@@ -0,0 +1,9 @@
+﻿
+using Confluent.Kafka;
+namespace Produtor.Config
+{
+    public class BrokerConfig : ProducerConfig
+    {
+        public string bootstrapServer = "Colocar aqui o enderço de onde o broker esta rodando";
+    }
+}
