386b44f46c74562cd593dbae128b5b458cc6cea1
Vitor Ferreira frança <vitorferreirafranca@gmail.com>
2025-08-20 14:01:49 -0300

    Adicionado consumidor

---
+++ consumidor/consumidor/consumidor/Program.cs
@@ -0,0 +1,28 @@
+using consumidor.config;
+using consumidor.service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ConsumerConfiguration>();
+builder.Services.AddHostedService<KafkaConsumerService>();//hosted : um seviço que roda em background, essencial para aplicações que precisam consumir mensagens de forma contínua, como é o caso de um consumidor Kafka.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ consumidor/consumidor/consumidor/config/ConsumerConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Confluent.Kafka;
+
+namespace consumidor.config
+{
+    public class ConsumerConfiguration : ConsumerConfig
+    {
+        public ConsumerConfiguration()
+        {
+            BootstrapServers = "localhost:9092";
+            GroupId = "grupo1";
+            AutoOffsetReset = Confluent.Kafka.AutoOffsetReset.Earliest;
+        }
+    }
+}
